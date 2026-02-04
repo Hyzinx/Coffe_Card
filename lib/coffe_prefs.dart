c129f3957b6a47eab471fd4a441d4db8b4809a88
Hyzinx <hyzin633@gmail.com>
2026-02-04 13:49:49 +0200

    Add Image in pubspec and use BlendMode.Multiply To Remove The Image BackColore and Make Colore same The Background Image

--- lib/coffe_prefs.dart
+++ lib/coffe_prefs.dart
@@ -7,8 +7,32 @@ class CoffePrefs extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Row(children: [Text("Strenghth: "), Text("3"), SizedBox(width: 50)]),
-        Row(children: [Text("Sugars: "), Text("3"), SizedBox(width: 50)]),
+        Row(
+          children: [
+            Text("Strenghth: "),
+            Text("3"),
+            SizedBox(width: 50),
+            Image.asset(
+              "assets/images/coffee_bean.png",
+              width: 25,
+              color: Colors.brown[100],
+              colorBlendMode: BlendMode.multiply,
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            Text("Sugars: "),
+            Text("3"),
+            SizedBox(width: 50),
+            Image.asset(
+              "assets/images/sugar_cube.png",
+              width: 25,
+              color: Colors.brown[100],
+              colorBlendMode: BlendMode.multiply,
+            ),
+          ],
+        ),
       ],
     );
   }
