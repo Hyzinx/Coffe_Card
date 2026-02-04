4d107d1f3e395be8b06a6590535dae9e79670f00
Hyzinx <hyzin633@gmail.com>
2026-02-04 12:12:31 +0200

    add Home In Another File

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:coffe_card/home.dart';
 
 void main() {
   runApp(MaterialApp(home: Home()));
-}
-
-class Home extends StatelessWidget {
-  const Home({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("My Coffe Id", style: TextStyle(color: Colors.white)),
-        backgroundColor: Colors.brown[700],
-      ),
-      body: Text("Hello In My Coffe Shop"),
-    );
-  }
 }
