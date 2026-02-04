c7ef444980e44d8cb7af94a5d56b2d3665a39c7b
Hyzinx <hyzin633@gmail.com>
2026-02-04 12:07:15 +0200

    Refactor Code

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("My Coffe Id", style: TextStyle(color: Colors.white)),
-          backgroundColor: Colors.brown[700],
-        ),
-        body: Home(),
-      ),
-    ),
-  );
+  runApp(MaterialApp(home: Home()));
 }
 
 class Home extends StatelessWidget {
@@ -19,22 +9,12 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blueGrey,
-      // width: 200,
-      // height: 100,
-      padding: EdgeInsets.symmetric(horizontal: 20),
-      margin: EdgeInsets.fromLTRB(12, 32, 1, 12),
-      child: Text(
-        "Hello In My Coffe Shop",
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 18,
-          letterSpacing: 4,
-          decoration: TextDecoration.underline,
-          fontStyle: FontStyle.italic
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Coffe Id", style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.brown[700],
       ),
+      body: Text("Hello In My Coffe Shop"),
     );
   }
 }
