8645ee388c659983902f7e8984e1c48c0a12b09d
Hyzinx <hyzin633@gmail.com>
2026-02-04 13:05:15 +0200

    The Colum widget | Main and Cross Access Alignment

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,16 @@ class SandBox extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Sandbox"), backgroundColor: Colors.grey),
-      body: Column(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(width: 100, color: Colors.red, child: Text('One')),
+          Container(width: 150, color: Colors.green, child: Text('Two')),
+          Container(width: 200, color: Colors.blue, child: Text('Three')),
+          Container(width: 100, color: Colors.yellow, child: Text('Four')),
+        ],
+      ),
     );
   }
 }
