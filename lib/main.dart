7fc86f4bc78d9dbe64917d37eb1da3ce19532d47
Hyzinx <hyzin633@gmail.com>
2026-02-04 12:50:02 +0200

    add Sandbox Environment To Try New Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:coffe_card/home.dart';
 
 void main() {
-  runApp(MaterialApp(home: Home()));
+  runApp(MaterialApp(home: SandBox()));
+}
+
+class SandBox extends StatelessWidget {
+  const SandBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Sandbox"), backgroundColor: Colors.grey),
+      body: Column(),
+    );
+  }
 }
