dc52be69a6885d2ef1bf72c90e08dc0fc67b4229
Hyzinx <hyzin633@gmail.com>
2026-02-04 11:53:14 +0200

    Container In Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,24 @@ void main() {
           title: Text("My Coffe Id", style: TextStyle(color: Colors.white)),
           backgroundColor: Colors.brown[700],
         ),
-        body: Center(
-          child: Text("Hello In My Coffe Shop", style: TextStyle(fontSize: 20)),
-        ),
+        body: Home(),
       ),
     ),
   );
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blueGrey,
+      // width: 200,
+      // height: 100,
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      margin: EdgeInsets.fromLTRB(12, 32, 1, 12),
+      child: Text("Hello In My Coffe Shop",style: TextStyle(color: Colors.white),),
+    );
+  }
 }
